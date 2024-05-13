57e3f6f0c216b97d6954d4350df21ba653deb76c
JorshSlimming <slimmingjorge@gmail.com>
2024-05-12 22:13:30 -0400

    Create class Empleado

---
+++ Programa/src/main/java/Paquete/Empleado.java
@@ -0,0 +1,11 @@
+package Paquete;
+
+public class Empleado {
+    private String id;
+    private String apellido;
+    private String nombre;
+    private String correo;
+
+    public Empleado(){  }
+    
+}
