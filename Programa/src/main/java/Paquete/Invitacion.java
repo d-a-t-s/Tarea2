46b57212dbd23dc640252a64f74e699afefa559d
ba-hc <benjahc1905@gmail.com>
2024-05-12 22:27:41 -0400

    Invitacion

---
+++ Programa/src/main/java/Paquete/Invitacion.java
@@ -0,0 +1,11 @@
+package Paquete;
+
+class Invitacion {
+    private String empleado;
+    private Date horaInvitacion;
+
+    public Invitacion (String empleado,Date horaInvitacion){
+        this.empleado = empleado;
+        this.horaInvitacion = horaInvitacion;
+    }
+}
