473b64467626941bd555fcabc50d3f8ac6554c3e
ba-hc <benjahc1905@gmail.com>
2024-05-15 00:09:18 -0400

    Creacion excepciones invitacion.java

---
+++ src/main/java/org/example/InvitacionException.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class InvitacionException extends Exception {
+
+    public InvitacionException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/org/example/InvitacionTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvitacionTest {
+    @Test
+    void test1(){
+        var test1 = new Invitacion();
+    }
+}
