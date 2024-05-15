0580848efbf282f2b9249555c0978b092cea6b31
ba-hc <benjahc1905@gmail.com>
2024-05-15 00:37:29 -0400

    InvitacionTest

--- src/main/java/org/example/Invitacion.java
+++ src/main/java/org/example/Invitacion.java
@@ -8,6 +8,10 @@ public class Invitacion {
     public Invitacion(Reunion reunion, Invitable invitado) throws InvitacionException {
         if (reunion == null && invitado == null) {
             throw new InvitacionException("Ambos la reunión y el invitado no pueden ser nulos.");
+        } else if (reunion == null) {
+            throw new InvitacionException("La reunión no puede ser nula.");
+        } else if (invitado == null) {
+            throw new InvitacionException("El invitado no puede ser nulo.");
         }
         this.reunion = reunion;
         this.invitado = invitado;
