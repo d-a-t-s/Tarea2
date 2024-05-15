074d643281de3bacd3f51b8e51a76ea199ab16a0
ba-hc <benjahc1905@gmail.com>
2024-05-14 20:42:24 -0400

    Update Invitacion.java

--- Programa/src/main/java/Paquete/Invitacion.java
+++ Programa/src/main/java/Paquete/Invitacion.java
@@ -1,11 +1,34 @@
-package Paquete;
 
+import java.time.Instant;
+
+/**
+ * Clase que representa una invitación a una reunión.
+ */
 class Invitacion {
-    private String empleado;
-    private Date horaInvitacion;
 
-    public Invitacion (String empleado,Date horaInvitacion){
-        this.empleado = empleado;
-        this.horaInvitacion = horaInvitacion;
+    /** Hora en la que se envió la invitación. */
+    private Instant hora;
+
+    /**
+     * Constructor de la clase Invitacion.
+     * @param hora La hora en la que se envió la invitación.
+     */
+    public Invitacion(Instant hora) {
+        this.hora = hora;
     }
-}
+
+    /**
+     * Obtiene la hora en la que se envió la invitación.
+     * @return La hora en la que se envió la invitación.
+     */
+    public Instant getHora() {
+        return hora;
+    }
+
+    /**
+     * Establece la hora en la que se envió la invitación.
+     * @param hora La nueva hora de la invitación.
+     */
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
